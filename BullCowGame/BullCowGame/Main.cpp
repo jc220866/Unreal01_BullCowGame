b2ff155397fe4e10ce6d60937216125240604155
Jakob <jakobcocking@outlook.com>
2018-12-01 11:38:26 +0000

    we give feedback on the player's guess. if they enter 'despacito', the guess will be repeated back to them infinitely

--- BullCowGame/BullCowGame/Main.cpp
+++ BullCowGame/BullCowGame/Main.cpp
@@ -1,20 +1,65 @@
 # include <iostream>
 # include <string>
 
+void PrintIntroduction();
+std::string GetPlayerGuess();
+void PrintGuessFeedback(std::string PlayerGuess);
+void BreakTheInternet(std::string PlayerGuess);
+
+constexpr int WORD_LENGTH = 5;
+
+// Only outputs strings, does nothing else
+void PrintIntroduction()
+{
+	std::cout << "Welcome to Bulls and Cows, a word guessing game!\n\n";
+
+	std::cout << "Every hidden word in this game is an isogram.\n";
+	std::cout << "An isogram is a word with no repeating letters.\n";
+	std::cout << "To play, simply guess a word.\n";
+	std::cout << "Then we will give you hints on how to improve your guess!\n";
+	std::cout << "If you get a COW, then one of your letters is right, but in the wrong place.\n";
+	std::cout << "If you get a BULL, then one of your letters is right AND in the right place.\n\n";
+}
+
+// This is the first thing our code does, the entrypoint of our program
 int main()
 {
-	constexpr int WORD_LENGTH = 5;
+	PrintIntroduction();
+	std::string Guess = GetPlayerGuess();
+	std::cout << "\n\nHello World\n\n";
+	BreakTheInternet(Guess);
 
-	std::cout << "Welcome to Bulls and Cows!\n\n";
-	std::cout << "Can you guess the " << WORD_LENGTH << " word I'm thinking of?\n\n";
+	return 0;
+}
+
+
+// Receives player's input for their guess
+std::string GetPlayerGuess()
+{
+	std::cout << "Can you guess the --> " << WORD_LENGTH << " <-- letter word I'm thinking of?\n";
+	std::cout << "________________________________________________________________________________\n\n";
 
 	std::string Guess = "";
 	while (Guess != "despacito")
-	{ 
+	{
 		std::cout << "Please enter your guess:\n";
-		std::getline (std::cin, Guess);
-		std::cout << "Your guess was: " << Guess << ".\n\n";
+		std::getline(std::cin, Guess);
+		PrintGuessFeedback(Guess);
 	}
+	return Guess;
+}
 
-	return 0;
+// After receiving the player's guess, we repeat it to them
+void PrintGuessFeedback(std::string PlayerGuess)
+{
+	std::cout << "Your guess was: " << PlayerGuess << "\n\n";
+}
+
+// When the player enters THAT word, we print it infinitely until the end of time and beyond
+void BreakTheInternet(std::string PlayerGuess)
+{
+	while (true)
+	{
+		std::cout << PlayerGuess << "\n";
+	}
 }
