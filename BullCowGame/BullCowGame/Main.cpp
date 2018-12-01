1d6b94381c49eb5418b6a8c44e9a6901bd3f7e10
Jakob <jakobcocking@outlook.com>
2018-12-01 10:37:49 +0000

    Added a prompt to get the user's guess

--- BullCowGame/BullCowGame/Main.cpp
+++ BullCowGame/BullCowGame/Main.cpp
@@ -1,7 +1,20 @@
 # include <iostream>
 # include <string>
 
-void main()
+int main()
 {
-	std::cout << "Hello World!\n:";
+	constexpr int WORD_LENGTH = 5;
+
+	std::cout << "Welcome to Bulls and Cows!\n\n";
+	std::cout << "Can you guess the " << WORD_LENGTH << " word I'm thinking of?\n\n";
+
+	std::string Guess = "";
+	while (Guess != "despacito")
+	{ 
+		std::cout << "Please enter your guess:\n";
+		std::getline (std::cin, Guess);
+		std::cout << "Your guess was: " << Guess << ".\n\n";
+	}
+
+	return 0;
 }
